0499d3785110604fb5a9d93c121eaeaec9523e80
LunaRibeiro <lunasantos@alunos.utfpr.edu.br>
2022-06-01 14:51:04 -0300

    Configurando o acesso com Auth0

--- vendas-app/src/pages/api/auth/[...nextauth].tsx
+++ vendas-app/src/pages/api/auth/[...nextauth].tsx
@@ -7,5 +7,11 @@ export default NextAuth({
             clientId: 'Iv1.8ee60ad29405212f',
             clientSecret: '7647b6af5fa2d1ec2bd489b0ebe92963f0d3d277'
         }), 
+
+        Providers.Auth0({
+            clientId: 'RDnXMxlxlwi8cFZh1xJx3L3BvUT5OJo2',
+            clientSecret: 'Ymu_V_s1kUDUBM1wlR1EiZ79-GPj9UWDVYZBQZ1LfZEpGr6ZV3l0ZoYJgb8TtB5E',
+            domain: 'dev-rhajd-n3.us.auth0.com'
+        }),
     ]
 })
