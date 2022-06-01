e794f111910b9fff9e1d3e1370b91b8e65e369bc
LunaRibeiro <lunasantos@alunos.utfpr.edu.br>
2022-06-01 14:00:48 -0300

    Instalado e configurado o Next Auth

---
+++ vendas-app/src/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+export default NextAuth({
+    providers: [
+        Providers.GitHub({
+            clientId: 'Iv1.8ee60ad29405212f',
+            clientSecret: '7647b6af5fa2d1ec2bd489b0ebe92963f0d3d277'
+        }),
+        Providers.Google({
+            
+        })
+    ]
+})
