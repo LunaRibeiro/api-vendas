e67660946f6e293ec4de1f7dc6303ad352a3d810
LunaRibeiro <lunasantos@alunos.utfpr.edu.br>
2022-06-01 14:18:47 -0300

    Logando na aplicação com GitHub

--- vendas-app/src/pages/api/auth/[...nextauth].tsx
+++ vendas-app/src/pages/api/auth/[...nextauth].tsx
@@ -6,9 +6,6 @@ export default NextAuth({
         Providers.GitHub({
             clientId: 'Iv1.8ee60ad29405212f',
             clientSecret: '7647b6af5fa2d1ec2bd489b0ebe92963f0d3d277'
-        }),
-        Providers.Google({
-            
-        })
+        }), 
     ]
 })
